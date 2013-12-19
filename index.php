eab42443fdb12b5d3ef3c4b2987e9f0b559c09e9
kentothemes <kentothemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-19 09:27:18 +0000

    Initial Upload
    git-svn-id: https://plugins.svn.wordpress.org/kento-vote/trunk@825403 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,253 @@
+<?php
+/*
+Plugin Name: Kento Vote
+Plugin URI: http://kentothemes.com
+Description: Kento Vote Plugin is count your vote and display voter thumbnail under vote button who voted on your post.
+Version: 1.0
+Author: KentoThemes
+Author URI: http://kentothemes.com
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+function kento_vote_latest_jquery() {
+	wp_enqueue_script('jquery');
+}
+add_action('init', 'kento_vote_latest_jquery');
+wp_enqueue_script('inkthemes', plugins_url( '/js/kento-vote.js' , __FILE__ ) , array( 'jquery' ));
+wp_localize_script( 'inkthemes', 'MyAjax', array( 'ajaxurl' => admin_url( 'admin-ajax.php')));
+define('KENTO_VOTE_PLUGIN_PATH', WP_PLUGIN_URL . '/' . plugin_basename( dirname(__FILE__) ) . '/' );
+wp_enqueue_style('kento-vote-style', KENTO_VOTE_PLUGIN_PATH.'css/style.css');
+register_activation_hook(__FILE__, kento_vote_install());
+Register_uninstall_hook(__FILE__, kento_vote_drop());
+function kento_vote_install() {
+    global $wpdb;
+        $sql = "CREATE TABLE IF NOT EXISTS " . $wpdb->prefix . "kento_vote"
+                 ."( UNIQUE KEY id (id),
+					id int(100) NOT NULL AUTO_INCREMENT,
+					postid  int(10) NOT NULL,
+					upvote  int(10) NOT NULL,
+					downvote  int(10) NOT NULL)";
+		$wpdb->query($sql);
+
+        $sql = "CREATE TABLE IF NOT EXISTS " . $wpdb->prefix . "kento_vote_info"
+                 ."( UNIQUE KEY id (id),
+					id int(100) NOT NULL AUTO_INCREMENT,
+					postid  int(10) NOT NULL,
+					userid  int(10) NOT NULL,				
+					votetype int(2) NOT NULL)";
+		$wpdb->query($sql);
+		}
+
+function kento_vote_drop() {
+	if ( get_option('kento_vote_deletion') == 1 ) {
+		
+		global $wpdb;
+		$wpdb->query('DROP TABLE IF EXISTS ' . $wpdb->prefix . 'kento_vote');
+		$wpdb->query('DROP TABLE IF EXISTS ' . $wpdb->prefix . 'kento_vote_info');
+	}
+}
+
+function is_user_logged($is_logged_in)
+
+	{
+		if ( is_user_logged_in() )
+		return "looged";
+		else
+		return "notlooged";
+
+	}
+function voted($post_id)
+
+	{
+
+	if ( is_user_logged_in() ) {
+		$postid = get_the_ID();
+		global $wpdb;
+		$userid = get_current_user_id();
+    	$table = $wpdb->prefix . "kento_vote_info";
+		
+		$wpdb->get_results("SELECT * FROM $table WHERE userid = '$userid' AND postid = '$postid'", ARRAY_A);
+		
+		if($wpdb->num_rows > 0 )
+			{
+				return "voted";
+			}
+			
+		else
+			{
+			return "notvoted";
+			}
+		}
+
+	}
+function voted_status($postid)
+	{
+	if ( is_user_logged_in() ) {
+		$postid = get_the_ID();
+		global $wpdb;
+		$userid = get_current_user_id();
+    	$table = $wpdb->prefix . "kento_vote_info";
+		
+		$result = $wpdb->get_results("SELECT votetype FROM $table WHERE userid = '$userid' AND postid = '$postid' ", ARRAY_A);
+		$voted_status = $result[0]['votetype'];
+		
+		if($voted_status==1){
+			return "upvoted";
+			}
+		elseif($voted_status==2){
+			return "downvoted";
+			}
+		}
+	}
+function who_voted($postid)
+	{
+		global $wpdb;
+    	$table = $wpdb->prefix . "kento_vote_info";
+		$result = $wpdb->get_results("SELECT userid FROM $table WHERE postid = '$postid' LIMIT 10", ARRAY_A);
+		$total_vote = $wpdb->num_rows;
+		for($i=0; $i<$total_vote; $i++)
+			{	
+				$userid.= get_avatar($result[$i]['userid'],100);
+			}
+		return $userid;
+	}
+function kento_vote_insert()
+	{
+	$postid = $_POST['postid'];
+	$votetype = $_POST['votetype'];
+    global $wpdb;
+    $table = $wpdb->prefix . "kento_vote";
+	
+	$wpdb->get_results( 'SELECT * FROM '.$table.' WHERE postid = "'.$postid.'"' );
+	
+	if($wpdb->num_rows > 0 )
+		{
+			if(voted($post_id)=="notvoted"){
+			if($votetype=="upvote")
+				{
+					$wpdb->query("UPDATE $table SET upvote = upvote+1 WHERE postid = '".$postid."'");
+					
+					kento_vote_info_insert($postid,$votetype);
+				}
+			elseif($votetype=="downvote")
+				{
+					$wpdb->query("UPDATE $table SET downvote = downvote+1 WHERE postid = '".$postid."'");
+					kento_vote_info_insert($postid,$votetype);
+				}
+				}
+		}
+	else 
+		{
+			if($votetype=="upvote")
+				{
+					$wpdb->query("INSERT INTO $table VALUES('',$postid,1,0)");
+					
+					global $wpdb;
+					$table = $wpdb->prefix . "kento_vote_info";
+					$postid = $postid;
+					$userid = get_current_user_id();
+					$votetype = "1";
+					$wpdb->query("INSERT INTO $table VALUES('',$postid,$userid,$votetype)");
+				}
+			elseif($votetype=="downvote")
+				{
+					$wpdb->query("INSERT INTO $table VALUES('',$postid,0,1)");
+					
+					global $wpdb;
+					$table = $wpdb->prefix . "kento_vote_info";
+					$postid = $postid;
+					$userid = get_current_user_id();
+					$votetype = "2";
+					$wpdb->query("INSERT INTO $table VALUES('',$postid,$userid,$votetype)");
+				}
+		}
+	die();
+	return true;
+	}
+function kento_vote_info_insert($postid,$votetype)
+	{
+		global $wpdb;
+		$table = $wpdb->prefix . "kento_vote_info";
+		$postid = $postid;
+		$votetype = $votetype;
+		$userid = get_current_user_id();
+		$wpdb->get_results("SELECT * FROM $table WHERE userid = '$userid' AND postid = '$postid'", ARRAY_A);
+		if($wpdb->num_rows > 0 )
+			{
+			}
+		else
+			{	if($votetype == "upvote")
+					{
+						$votetype = "1";
+						$wpdb->query("INSERT INTO $table VALUES('',$postid,$userid,$votetype)");
+					}
+				elseif($votetype == "downvote")
+					{
+						$votetype = "2";
+						$wpdb->query("INSERT INTO $table VALUES('',$postid,$userid,$votetype)");
+					}
+			}
+	}
+add_action('wp_ajax_kento_vote_insert', 'kento_vote_insert');
+add_action('wp_ajax_nopriv_kento_vote_insert', 'kento_vote_insert');
+function up_count($post_id)
+	{	
+		$postid = $post_id;
+		global $wpdb;
+    	$table = $wpdb->prefix . "kento_vote";
+		$result = $wpdb->get_results("SELECT upvote FROM $table WHERE postid = '$postid'", ARRAY_A);
+		$up_count = $result[0]['upvote'];
+		if($up_count==""){
+			return "0";
+			}
+		else{
+			return $up_count;
+			}
+	}
+function down_count($post_id)
+	{	
+		$postid = $post_id;
+		global $wpdb;
+    	$table = $wpdb->prefix . "kento_vote";
+		$result = $wpdb->get_results("SELECT downvote FROM $table WHERE postid = '$postid'", ARRAY_A);
+		$down_count = $result[0]['downvote'];
+		if($down_count==""){
+			return "0";
+			}
+		else{
+			return $down_count;
+			}
+	}
+
+//Login form Arguments
+ $args = array(
+        'echo' => true,
+        'redirect' => site_url( $_SERVER['REQUEST_URI'] ), 
+        'form_id' => 'loginform',
+        'label_username' => __( 'Username' ),
+        'label_password' => __( 'Password' ),
+        'label_remember' => __( 'Remember Me' ),
+        'label_log_in' => __( 'Log In' ),
+        'id_username' => 'user_login',
+        'id_password' => 'user_pass',
+        'id_remember' => 'rememberme',
+        'id_submit' => 'wp-submit',
+        'remember' => true,
+        'value_username' => NULL,
+        'value_remember' => false );
+function show_form($cont){
+$cont.= "<div id='kento-vote' class='".voted($post_id)."' logged='".is_user_logged($is_logged_in)."' >";
+$cont.=  "<strong>Vote on This!</strong><br /><br />";
+$cont.=   "<ul class='".voted_status($postid)."'><li id='kento-vote-up' votetype='upvote' postid='".get_the_ID()."' class='kento-vote-up vote-button' ><div class='kento-vote-info'><span class='up-vote-value' upvotevalue='".up_count(get_the_ID())."'>".up_count(get_the_ID())."</span> <span class='up-vote-text'>Up Vote</span></div></li>";
+$cont.=  "<li id='kento-vote-down' votetype='downvote' postid='".get_the_ID()."' class='kento-vote-down vote-button'  ><div class='kento-vote-info'><span class='down-vote-value' downvotevalue='".down_count(get_the_ID())."'>".down_count(get_the_ID())."</span><span class='down-vote-text'> Down Vote</span></div></li></ul>";
+$cont.= "<div class='who-voted'>".who_voted(get_the_ID())."</div>";
+$cont.= wp_login_form($args);
+$cont.= "<div class='login-bg'></div>";
+$cont.=  "</div>";
+if(is_single()){
+return $cont;
+}
+}
+add_filter('the_content', 'show_form');
+?>
---
+++ readme.txt
@@ -0,0 +1,51 @@
+=== Kento Vote ===
+Contributors: hasanrang05
+Donate link: 
+Tags:  post vote, wordpress vote, Vote It Up, Voting, Polls, thumbs up, Like Post, Thumbs up down
+Requires at least: 3.5
+Tested up to: 3.7.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Vote on Post and Display Who Voted via gravatar thumbnail.
+
+== Description ==
+
+Kento Vote Plugin is count your vote and display voter thumbnail under vote button who voted on your post.
+
+Plugin Features
+
+* Up/Down Vote.
+* Up/Down vote Count.
+* Display Thumbnail who voted on post.
+* Popup Login Box if user not logged. 
+* Current Vote Marker for logged in user Up/Down vote.
+* Unique Vote Count, if user voted will not able to vote again on same post.
+
+Live Preview: http://kentothemes.com/demos/kento-vote/kento-vote-plugin/
+
+== Installation ==
+
+Via WordPress -
+
+1. Install as regular WordPress plugin.
+2. Go your Pluings setting via WordPress Dashboard and activate it.
+3. This Plugin Doesn't have any setting page
+4. After Activate Visit your Single Post.
+
+
+== Screenshots ==
+
+1. Who Vote on post.
+2. Vote With Down Vote.
+3. Vote With UpVote.
+4. Login Box .
+5. Normal View.
+
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
+
